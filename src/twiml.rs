9e49331ed6161ce15254ce16ad0a96057840d36a
Oleg Orlov <orlov@enfint.ai>
2025-03-04 17:38:01 +0300

    avoid code duplicate

--- src/twiml.rs
+++ src/twiml.rs
@@ -36,173 +36,184 @@ mod elements {
     use super::*;
     use xml_builder::{XMLElement};
 
-    /// Say TwiML Element for text-to-speech
-    #[derive(Debug, Clone)]
-    pub struct Say {
-        text: String,
+    #[derive(Debug)]
+    struct ElementFactory {
+        element: String,
+        text: Option<String>,
         attributes: Vec<(String, String)>,
+        children: Vec<Box<dyn TwiMLElement>>,
+    }
+
+    impl ElementFactory {
+        /// Create a new ElementFactory element
+        pub fn new(element: impl Into<String>, text: Option<impl Into<String>>) -> Self {
+            Self {
+                element: element.into(),
+                text: if let Some(text) = text {Some(text.into())} else {None},
+                attributes: Vec::new(),
+                children: Vec::new(),
+            }
+        }
+    }
+
+    impl TwiMLElement for ElementFactory {
+        fn to_xml(&self) -> XMLElement {
+            let mut elem = XMLElement::new(&self.element);
+
+            // Add attributes
+            for (key, value) in &self.attributes {
+                elem.add_attribute(key, value);
+            }
+
+            // Add children
+            for child in &self.children {
+                elem.add_child(child.to_xml()).unwrap();
+            }
+
+            // Add text (use owned String)
+            if let Some(text) = self.text.clone() {
+                elem.add_text(text).unwrap();
+            }
+
+            elem
+        }
+    }
+
+    /// Say TwiML Element for text-to-speech
+    #[derive(Debug)]
+    pub struct Say {
+        factory: ElementFactory,
     }
 
     impl Say {
         /// Create a new Say element
         pub fn new(text: impl Into<String>) -> Self {
             Self {
-                text: text.into(),
-                attributes: Vec::new(),
+                factory: ElementFactory::new("Say", Some(text)),
             }
         }
 
         /// Set voice attribute
         pub fn voice(mut self, voice: impl Into<String>) -> Self {
-            self.attributes.push(("voice".to_string(), voice.into()));
+            self.factory.attributes.push(("voice".to_string(), voice.into()));
             self
         }
 
         /// Set language attribute
         pub fn language(mut self, language: impl Into<String>) -> Self {
-            self.attributes.push(("language".to_string(), language.into()));
+            self.factory.attributes.push(("language".to_string(), language.into()));
             self
         }
     }
 
-    impl super::TwiMLElement for Say {
+    impl TwiMLElement for Say {
         fn to_xml(&self) -> XMLElement {
-            let mut elem = XMLElement::new("Say");
-            
-            // Add attributes
-            for (key, value) in &self.attributes {
-                elem.add_attribute(key, value);
-            }
-            
-            // Add text (use owned String)
-            elem.add_text(self.text.clone()).unwrap();
-            
-            elem
+            self.factory.to_xml()
         }
     }
 
     /// Gather TwiML Element for collecting user input
     #[derive(Debug)]
     pub struct Gather {
-        attributes: Vec<(String, String)>,
-        children: Vec<Box<dyn super::TwiMLElement>>,
+        factory: ElementFactory,
     }
 
     impl Gather {
         /// Create a new Gather element
         pub fn new() -> Self {
             Self {
-                attributes: Vec::new(),
-                children: Vec::new(),
+                factory: ElementFactory::new("Gather", None::<String>),
             }
         }
 
         /// Set action attribute
         pub fn action(mut self, action: impl Into<String>) -> Self {
-            self.attributes.push(("action".to_string(), action.into()));
+            self.factory.attributes.push(("action".to_string(), action.into()));
             self
         }
 
         /// Set method attribute
         pub fn method(mut self, method: impl Into<String>) -> Self {
-            self.attributes.push(("method".to_string(), method.into()));
+            self.factory.attributes.push(("method".to_string(), method.into()));
             self
         }
 
         /// Set number of digits to collect
         pub fn num_digits(mut self, num: impl Into<String>) -> Self {
-            self.attributes.push(("numDigits".to_string(), num.into()));
+            self.factory.attributes.push(("numDigits".to_string(), num.into()));
             self
         }
 
         /// Add a Say child element
         pub fn say(mut self, say: Say) -> Self {
-            self.children.push(Box::new(say));
+            self.factory.children.push(Box::new(say));
             self
         }
     }
 
-    impl super::TwiMLElement for Gather {
+    impl TwiMLElement for Gather {
         fn to_xml(&self) -> XMLElement {
-            let mut elem = XMLElement::new("Gather");
-            
-            // Add attributes
-            for (key, value) in &self.attributes {
-                elem.add_attribute(key, value);
-            }
-            
-            // Add children
-            for child in &self.children {
-                elem.add_child(child.to_xml()).unwrap();
-            }
-            
-            elem
+            self.factory.to_xml()
         }
     }
 
     /// Redirect TwiML Element
-    #[derive(Debug, Clone)]
+    #[derive(Debug)]
     pub struct Redirect {
-        url: String,
+        factory: ElementFactory,
     }
 
     impl Redirect {
         /// Create a new Redirect element
         pub fn new(url: impl Into<String>) -> Self {
-            Self { url: url.into() }
+            Self {
+                factory: ElementFactory::new("Redirect", Some(url)),
+            }
         }
     }
 
-    impl super::TwiMLElement for Redirect {
+    impl TwiMLElement for Redirect {
         fn to_xml(&self) -> XMLElement {
-            let mut elem = XMLElement::new("Redirect");
-            // Use cloned owned String
-            elem.add_text(self.url.clone()).unwrap();
-            elem
+            self.factory.to_xml()
         }
     }
 
     /// Response TwiML Element
     #[derive(Debug)]
     pub struct Response {
-        children: Vec<Box<dyn super::TwiMLElement>>,
+        factory: ElementFactory,
     }
 
     impl Response {
         /// Create a new Response element
         pub fn new() -> Self {
-            Self { children: Vec::new() }
+            Self {
+                factory: ElementFactory::new("Response", None::<String>),
+            }
         }
 
         /// Add a Say child element
         pub fn say(mut self, say: Say) -> Self {
-            self.children.push(Box::new(say));
+            self.factory.children.push(Box::new(say));
             self
         }
 
         /// Add a Gather child element
         pub fn gather(mut self, gather: Gather) -> Self {
-            self.children.push(Box::new(gather));
+            self.factory.children.push(Box::new(gather));
             self
         }
 
         /// Add a Redirect child element
         pub fn redirect(mut self, url: impl Into<String>) -> Self {
-            self.children.push(Box::new(Redirect::new(url)));
+            self.factory.children.push(Box::new(Redirect::new(url)));
             self
         }
     }
 
-    impl super::TwiMLElement for Response {
+    impl TwiMLElement for Response {
         fn to_xml(&self) -> XMLElement {
-            let mut elem = XMLElement::new("Response");
-            
-            // Add children
-            for child in &self.children {
-                elem.add_child(child.to_xml()).unwrap();
-            }
-            
-            elem
+            self.factory.to_xml()
         }
     }
 }
